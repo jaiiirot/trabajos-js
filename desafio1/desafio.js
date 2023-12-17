6be4db04154717a8a3573d1e298c20770e306ac6
Jhon Jairo Tumiri <jhon.jairo.tumiri@gmail.com>
2023-12-17 00:45:56 -0300

    entregar

--- desafio1/desafio.js
+++ desafio1/desafio.js
@@ -23,15 +23,27 @@ Archivo de Javascript listo para ejecutarse desde node.
 */
 
 class ProductManager {
+  #codigo = 0;
   constructor() {
     this.productos = [];
   }
+  addProduct = ({ title, descripcion, price, thumbnail, stock }) => {
+    if (typeof title !== "string" || title.length === 0)
+      return console.log("titulo no es un string y campo obligatorio");
+    if (typeof descripcion !== "string" || descripcion.length === 0)
+      return console.log("descripcion no es un string y campo obligatorio");
+    if (typeof thumbnail !== "string" || thumbnail.length === 0)
+      return console.log("thumbnail no es una direccion y campo obligatorio");
+    if (typeof price !== "number")
+      return console.log("price no es un numero y campo obligatorio");
+    if (typeof stock !== "number")
+      return console.log("stock no es un numero y campo obligatorio");
 
-  addProduct = (title, descripcion, price, thumbnail, code, stock) => {
-    if (typeof price !== "number") return console.log("price no es un numero");
-    if (typeof stock !== "number") return console.log("stock no es un numero");
+    this.#codigo++;
     if (this.productos.length !== 0) {
-      const existe = this.productos.some((producto) => producto.code === code);
+      const existe = this.productos.some(
+        (producto) => producto.code === this.#codigo
+      );
       if (existe === true)
         return console.log(`Un producto ya tiene el codigo: ${code}`);
     }
@@ -40,7 +52,7 @@ class ProductManager {
       descripcion,
       price,
       thumbnail,
-      code,
+      code: this.#codigo,
       stock,
     });
   };
@@ -61,30 +73,34 @@ class ProductManager {
 
 let producto1 = new ProductManager();
 
-producto1.addProduct(
-  "producto prueba",
-  "Este es un producto prueba",
-  200,
-  "Sin imagen",
-  "abc123",
-  25
-);
-producto1.addProduct(
-  "Short",
-  "descripcion del short",
-  200,
-  "ruta 2",
-  "abc123",
-  20
-);
-producto1.addProduct(
-  "Pantalon",
-  "descripcion del pant",
-  100,
-  "ruta 1",
-  "vad213",
-  10
-);
+producto1.addProduct({
+  title: 0,
+  descripcion: "Este es un producto prueba",
+  price: 200,
+  thumbnail: "Sin imagen",
+  stock: 25,
+});
+producto1.addProduct({
+  title: "Short",
+  descripcion: "",
+  price: 200,
+  thumbnail: "ruta 2",
+  stock: 20,
+});
+producto1.addProduct({
+  title: "Pantalon",
+  descripcion: "descripcion del pant",
+  price: "asd",
+  thumbnail: "ruta 1",
+  stock: 10,
+});
+producto1.addProduct({
+  title: "Prueba",
+  descripcion: "Este es un producto prueba",
+  price: 200,
+  thumbnail: "Sin imagen",
+  stock: 25,
+});
 producto1.getProducts();
-producto1.getProductsById(8);
+producto1.getProductsById(1);
 producto1.getProductsById("abc123");
